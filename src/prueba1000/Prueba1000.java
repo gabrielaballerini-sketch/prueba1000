70cf4678ac5e6999ae185fea50b97469a8379e5b
Gabriela Ballerini <gabrielaballerini@gmail.com>
2025-08-25 16:52:05 -0300

    Initial commit

---
+++ src/prueba1000/Prueba1000.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba1000;
+
+/**
+ *
+ * @author Capotes
+ */
+public class Prueba1000 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("fdsfdsfds");
+        System.out.println("");
+    }
+    
+}
